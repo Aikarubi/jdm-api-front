05f4bcb6cbfc2a5680b73e35c7c87817a81f5ae1
Aikarubi <nerupe@floridauniversitaria.es>
2025-03-11 12:05:00 +0100

    rutas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './views/home/home.component';
+import { DocsLayoutComponent } from './layouts/docs-layout/docs-layout.component';
+import { DocsComponent } from './views/docs/docs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+        ]
+    },
+    {
+        path: 'dcos',
+        component: DocsLayoutComponent,
+        children: [
+            { path: '', component: DocsComponent },
+        ]
+    }
+];
