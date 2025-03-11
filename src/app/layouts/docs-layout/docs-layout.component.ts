0a6b4c14e5d3d533793698026b57357900741544
Aikarubi <nerupe@floridauniversitaria.es>
2025-03-11 12:02:14 +0100

    layouts configurados

--- src/app/layouts/docs-layout/docs-layout.component.ts
+++ src/app/layouts/docs-layout/docs-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CopyBarComponent } from '../../components/copy-bar/copy-bar.component';
 
 @Component({
   selector: 'app-docs-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, FooterComponent, CopyBarComponent],
   templateUrl: './docs-layout.component.html',
   styleUrl: './docs-layout.component.css'
 })
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CopyBarComponent } from '../../components/copy-bar/copy-bar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CopyBarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
