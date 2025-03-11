41880850c95d1549dcc492692b50dc825d515248
Aikarubi <nerupe@floridauniversitaria.es>
2025-03-11 12:00:44 +0100

    views html y css

--- src/app/views/docs/docs.component.ts
+++ src/app/views/docs/docs.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-docs',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './docs.component.html',
   styleUrl: './docs.component.css'
 })
