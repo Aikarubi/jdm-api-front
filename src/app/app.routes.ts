846e2d9fc3768aab70c575ef1098f2d1d114737c
Aikarubi <nerupe@floridauniversitaria.es>
2025-03-12 18:35:06 +0100

    modificando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,8 +10,7 @@ export const routes: Routes = [
         path: '',
         component: MainLayoutComponent,
         children: [
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
+            { path: '', component: HomeComponent },
         ]
     },
     {
